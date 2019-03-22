e672dded4585307fd58c742c3b0bb81ef971833b
tom <8497308+tominal@users.noreply.github.com>
2019-03-21 23:14:24 -0500

    Update server_hop.lua
    you're trying to run console commands on the server

--- lua/autorun/server/server_hop.lua
+++ lua/autorun/server/server_hop.lua
@@ -1,7 +1,7 @@
 hook.Add("PlayerSay", "server_hopper", function(ply, text, public)
 	text = string.lower(text)
 	if ( text == "!hop" || text == "!servers"  || text == "!hopper" ) then
-		RunConsoleCommand("server_hop")
+		ply:ConCommand("server_hop")
 		return ""
 	end
 end )
